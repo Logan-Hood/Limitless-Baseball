636b43d9dcab0e31d84a3e1c70e5d7575963d003
Logan-Hood <logan.hood42@gmail.com>
2024-12-12 15:32:57 -0500

    Upload 3

--- js/index.js
+++ js/index.js
@@ -7,3 +7,26 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+
+
+
+
+
+
+
+
+
+// winter clinic overlay
+document.addEventListener('DOMContentLoaded', function () {
+    const winterClinicBtn = document.querySelector('.winter-clinic-btn');
+    const winterClinicOverlay = document.getElementById('winter-clinic-overlay');
+    const closeOverlayBtn = document.querySelector('.close-overlay-btn');
+  
+    winterClinicBtn.addEventListener('click', function () {
+      winterClinicOverlay.classList.add('show');
+    });
+  
+    closeOverlayBtn.addEventListener('click', function () {
+      winterClinicOverlay.classList.remove('show');
+    });
+  });
